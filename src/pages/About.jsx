746a7fb921900e4e464898e5abced057f03941a5
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2024-11-23 22:48:56 +0700

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import '../assets/About.css';
 const About = () => {
     return (
         <div className="about-page">
-            <h1>About Me</h1>
+            <h1>About Mfffe</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repellendus autem sit unde dolor!</p>
         </div>
     );
